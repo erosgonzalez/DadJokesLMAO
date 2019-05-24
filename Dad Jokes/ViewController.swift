7c9ce7f9f05002f459440bb62b79020fec33fa73
erosgonzalez <egonzalez-lopez@csumb.edu>
2019-05-24 02:18:44 -0700

    Added assets and design

--- Dad Jokes/ViewController.swift
+++ Dad Jokes/ViewController.swift
@@ -12,6 +12,8 @@ import Alamofire
 
 class ViewController: UIViewController {
     @IBOutlet weak var jokeLabel: UILabel!
+    @IBOutlet weak var jokeButton: UIButton!
+    
     let url = URL(string: "https://icanhazdadjoke.com/")!
     let headers = [
         "Accept": "application/json"
@@ -24,7 +26,6 @@ class ViewController: UIViewController {
     }
     
     @IBAction func onJoke(_ sender: Any) {
-        print (Alamofire.request(url))
         Alamofire.request(url, headers: headers).responseJSON{ response in
             // check for errors
             guard response.result.error == nil else {
@@ -35,22 +36,20 @@ class ViewController: UIViewController {
             }
             // make sure we got some JSON since that's what we expect
             guard let json = response.result.value as? [String: Any] else {
-                print("didn't get todo object as JSON from API")
+                print("didn't get joke JSON from API")
                 if let error = response.result.error {
                     print("Error: \(error)")
                 }
                 return
             }
-            // get and print the title
-            guard let todoTitle = json["joke"] as? String else {
-                print("Could not get todo title from JSON")
+            guard let jokeJSON = json["joke"] as? String else {
+                print("Could not get joke")
                 return
             }
-            print(todoTitle)
-        }
-            // check for errors
+            //joke retrieved and set to label
+            self.jokeLabel.text = jokeJSON
             
-        
+        }
     }
     
 
