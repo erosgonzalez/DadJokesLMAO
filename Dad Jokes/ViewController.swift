5a2992b8ce6c472166a6beacad5f3bdb059f69a9
erosgonzalez <egonzalez-lopez@csumb.edu>
2019-05-23 22:48:00 -0700

    Fixed json retrieving

--- Dad Jokes/ViewController.swift
+++ Dad Jokes/ViewController.swift
@@ -8,10 +8,15 @@
 
 import UIKit
 import Foundation
+import Alamofire
 
 class ViewController: UIViewController {
     @IBOutlet weak var jokeLabel: UILabel!
     let url = URL(string: "https://icanhazdadjoke.com/")!
+    let headers = [
+        "Accept": "application/json"
+    ]
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,25 +24,33 @@ class ViewController: UIViewController {
     }
     
     @IBAction func onJoke(_ sender: Any) {
-        var request = URLRequest(url: url)
-        
-        request.addValue("application/json", forHTTPHeaderField: "Accept")
-        let task = URLSession.shared.dataTask(with: url) { data, response, error in
-            guard error == nil else {
-                print(error!)
+        print (Alamofire.request(url))
+        Alamofire.request(url, headers: headers).responseJSON{ response in
+            // check for errors
+            guard response.result.error == nil else {
+                // got an error in getting the data, need to handle it
+                print("error calling GET")
+                print(response.result.error!)
                 return
             }
-            guard let data = data else {
-                print("Data is empty")
+            // make sure we got some JSON since that's what we expect
+            guard let json = response.result.value as? [String: Any] else {
+                print("didn't get todo object as JSON from API")
+                if let error = response.result.error {
+                    print("Error: \(error)")
+                }
                 return
             }
-            print (data)
-            print (response)
-            //let joke = try! JSONSerialization.jsonObject(with: data)
-            //print(joke)
-            //print(response)
+            // get and print the title
+            guard let todoTitle = json["joke"] as? String else {
+                print("Could not get todo title from JSON")
+                return
+            }
+            print(todoTitle)
         }
-        task.resume()
+            // check for errors
+            
+        
     }
     
 
